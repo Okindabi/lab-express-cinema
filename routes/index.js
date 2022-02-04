d8a6571912fc4b95d351ec065fe58a728f2c97af
Nicholas Badiola <NicholasBadiola@gmail.com>
2022-02-04 09:32:13 -0500

    done, needs finishing styling touches

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
+const Movie = require("../models/Movie.model");
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
 
+router.get("/movies", function(req, res) {
+    Movie.find()
+        .then((results) => {
+            console.log("movies found:", results);
+            res.render("movies", { movies: results });
+        })
+        .catch((err) => {
+            console.log("Something went wrong", err);
+        });
+});
+
+router.get("/movie/:movieId", function(req, res) {
+    Movie.findById(req.params.movieId)
+        .then((results) => {
+            console.log("movie was found:", results);
+            res.render("movie", results);
+        })
+        .catch((err) => {
+            console.log("Something went wrong", err);
+        });
+});
 module.exports = router;
